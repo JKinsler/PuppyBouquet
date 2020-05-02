f071b2afd4236815182a893a61bbac2911d5fcd0
Johanna Kinsler <johanna.kinsler@gmail.com>
2020-05-02 23:21:43 +0000

    created function send_sms

--- send_sms.py
+++ send_sms.py
@@ -20,10 +20,20 @@ vid_img = pups.choose_image()
 
 account_sid = os.environ["TWILIO_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-
+jk_num = os.environ["JK_PHONE_NUM"]
 client = Client(account_sid, auth_token)
-client.messages.create(
-    to=os.environ["JK_PHONE_NUM"],
+
+
+def send_sms(phone_num, msg=vid_img):
+    """Send an sms message to the phone number which is given as an input 
+    parameter"""
+
+    client.messages.create(
+    to=jk_num,
     from_=os.environ["TWILIO_PHONE_NUM"], 
-    body=vid_img
+    body=msg
     )
+
+    return "message sent to {phone_num}, {msg}".format(phone_num = phone_num,\
+                                                        msg = msg)
+
